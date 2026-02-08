b948c2aa6b29bd962b03ea2a6d6c1f745ad2b149
Ditero22 <karl.ortega.2002.ko@gmail.com>
2026-02-08 22:43:43 +0800

    Add base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Practice/",
+  base: "/https://ditero22.github.io/Practice/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
